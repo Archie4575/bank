d2c5936b8bc8ae624c4bbb11a09633b682819e2b
Archer Fabling <archie4575@gmail.com>
2023-04-26 19:02:20 +0800

    Read c_file to linked list and print

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 CC = gcc
 CFLAGS = -Wall -Werror -ansi -pedantic -g
 EXEC = cq
-OBJ = main.o
+OBJ = main.o linkedlist.o
 
 $(EXEC): $(OBJ)
 	$(CC) $(OBJ) -o $(EXEC)
 
 main.o: main.c
 	$(CC) $(CFLAGS) -pthread -c main.c
+
+linkedlist.o: linkedlist.c linkedlist.h
+	$(CC) $(CFLAGS) -c linkedlist.c
--- main.c
+++ main.c
@@ -2,27 +2,66 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include "linkedlist.h"
 
-void* customer(void* arg) {
-	int* n = malloc(4);
-	*n = 1;
-	while (*n<5) {
-		printf("%i\n", *n);
-		*n += 1;
-		sleep(2);
-	}
-	return n;
+#define C_FILE "c_file.txt"
+
+/* Customer Code */
+typedef struct {
+    int n;
+    char type;
+} customer_t;
+
+customer_t* create_customer() {
+	customer_t* customer = (customer_t*) malloc(sizeof(customer_t));
+	customer->n = 0;
+	customer->type = ' ';
+	return customer;
 }
 
-int main (int argc, char** argv) {
-	pthread_t c1;
-	void* retval = NULL;
-	
-	pthread_create(&c1, NULL, &customer, NULL);
+void print_customer(void* customer) {
+	customer_t* c = (customer_t*)customer;
+	printf("#%i: '%c'\n", c->n, c->type);
+	return;
+}
 
-	pthread_join(c1, &retval);
-	printf("Returned: %i\n", *(int*)retval);
-	free(retval);
+void* customer(void* queue) {
+	LinkedList* c_queue = (LinkedList*) queue;
+	FILE* file = fopen(C_FILE, "r");
+	customer_t* customer = NULL;
+
+	if (file == NULL ) {
+		perror("Error reading file.");
+	} else {
+		while(!feof(file)) {
+			customer = create_customer();
+			fscanf(file, "%i %c", &customer->n, &customer->type);
+			insertLast(c_queue, customer);
+		}
+	}
+
+	printLinkedList(c_queue, &print_customer);
+
+	freeLinkedList(c_queue, &free);
+	return NULL;
+}
+/* End of customer code */
+
+/* Teller Code */
+typedef struct {
+	int n; /* Teller number */
+	int served; /* Customers served tally */
+} teller_t;
+/* End of teller code */
+
+int main (int argc, char** argv) {
+	LinkedList* c_queue = createLinkedList();
+
+	pthread_t customer_th;
+	pthread_create(&customer_th, NULL, &customer, c_queue);
+
+
+	pthread_join(customer_th, NULL);
 
 	return 0;
 }
