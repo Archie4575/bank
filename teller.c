7226e70f5954c31f818a20857245b8779c124127
Archer Fabling <archie4575@gmail.com>
2023-05-06 14:15:49 +0800

    fixed bug, fully functional multi-threading

--- teller.c
+++ teller.c
@@ -22,28 +22,34 @@ void* teller(void* arg) {
         return NULL;
     }
     
-    
+    pthread_mutex_lock(&queue->lock);
     while (TRUE) {
-
-        pthread_cond_wait(&queue->new_customer, &queue->lock);
+        /* If no customers in queue, block */
+        if (queue->list->length <= 0) {
+            pthread_cond_wait(&queue->new_customer, &queue->lock);
+        }
+        /* Pop last customer from queue and signal not_full */
         customer_t* customer = (customer_t*) removeLast(queue->list);
-
-        printf("T%i: serving C%i%c\n", teller_id, customer->n, customer->type);
-
-        pthread_mutex_unlock(&queue->lock);
         pthread_cond_signal(&queue->not_full);
+        pthread_mutex_unlock(&queue->lock);
 
+        /* Serve customer */
+        printf("T%i: serving C%i%c\n", teller_id, customer->n, customer->type);
         teller_serve(params, teller_id, customer);
         free(customer);
 
+        /* Update total customers served */
         increment_tallies(params, teller_id);
 
+        /* Terminate if there are no more incoming customers */
         pthread_mutex_lock(&queue->lock);
         if (!queue->incoming) break;
-
     }
     pthread_mutex_unlock(&queue->lock);
-    printf("Teller %i terminating.\n", teller_id);
+
+    pthread_mutex_lock(&params->queue->lock);
+    printf("Teller %i terminating, served %i customers\n", teller_id, params->totals->tallies[teller_id-1]);
+    pthread_mutex_unlock(&params->queue->lock);
 
     return NULL;
 }
